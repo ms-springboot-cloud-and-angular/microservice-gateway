dd3c75debb56c6ccc731c39c2a894de4f6586df5
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-14 12:55:56 +0200

    New SpringBoot project - Gateway

---
+++ src/main/java/com/joseluisestevez/ms/app/gateway/MicroserviceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceGatewayApplication.class, args);
+    }
+
+}
